a39f04588d7df3b89e61883fa732fa7a13a95d42
Audrey-Okumu <okumuaudrey71@gmail.com>
2025-09-18 21:19:23 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -34,7 +34,7 @@ class TestAccessNestedMap(unittest.TestCase):
 class TestGetJson(unittest.TestCase):
     """Unit tests for utils.get_json"""
 
-    @patch("utils.get_json.requests.get")
+    @patch("utils.requests.get")
     def test_get_json(self, mock_get):
         """Test get_json returns the expected payload"""
         test_cases = [
