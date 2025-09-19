a8ec783433691a86f7db647ea8103d15dd297ce1
Audrey-Okumu <okumuaudrey71@gmail.com>
2025-09-19 21:43:20 +0300

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  # <--- Added this line
+    path('api/', include('chats.urls')),  # Included chats API endpoints
+    path('api-auth/', include('rest_framework.urls')),  # Included DRF auth endpoints
 ]
