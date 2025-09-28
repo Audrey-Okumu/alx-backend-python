c6d52ce4b5f2983f20241999b11c10c0c11c06c3
Audrey <audreyokumu@gmail.com>
2025-09-28 13:20:31 +0300

    configuring middleware settings

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -78,7 +78,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    # custom middleware
     "chats.middleware.RequestLoggingMiddleware",
 ]
 
