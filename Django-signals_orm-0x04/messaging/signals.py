9af66adb0dceba1abbf6dd862c31054faaf7bdf1
Audrey-Okumu <okumuaudrey71@gmail.com>
2025-10-03 18:25:03 +0300

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
 
@@ -27,3 +27,18 @@ def log_message_edit(sender, instance, **kwargs):
                 instance.edited = True
         except Message.DoesNotExist:
             pass
+
+@receiver(post_delete, sender=User)
+def cleanup_user_related_data(sender, instance, **kwargs):
+    """
+    When a User is deleted, remove their related data:
+    - Messages (sent and received)
+    - Notifications
+    - MessageHistory
+    """
+    # Delete messages sent or received
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete notifications for the user
+    Notification.objects.filter(user=instance).delete()
