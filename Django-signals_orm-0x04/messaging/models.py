61413e4ad2eac8d8a9b2cef57be3e0b2472d650f
Audrey-Okumu <okumuaudrey71@gmail.com>
2025-10-03 18:13:30 +0300

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -19,3 +19,11 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user.username} - Message {self.message.id}"
+        
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name="history")
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History for Message {self.message.id} at {self.edited_at}"
